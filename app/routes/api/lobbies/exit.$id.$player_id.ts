06e04741d92c227d4a4e394e38abb2716c7add55
neilbb <bn0010100@gmail.com>
2025-02-09 17:46:50 -0600

    refactor inputs of DELETE api

--- app/routes/api/lobbies/exit.$id.$player_id.ts
+++ app/routes/api/lobbies/exit.$id.$player_id.ts
@@ -3,20 +3,18 @@ import { createAPIFileRoute } from '@tanstack/start/api'
 import { api } from '~/../convex/_generated/api'
 import { client } from '~/sharedConvex'
 
-export const APIRoute = createAPIFileRoute('/api/lobbies/exit/$id')({
-  DELETE: async ({ params, request }) => {
-    const { id } = params
-    const body = await request.json()
+export const APIRoute = createAPIFileRoute('/api/lobbies/exit/$id/$player_id')({
+  DELETE: async ({ params }) => {
+    const { id, player_id } = params
 
     await client.mutation(api.lobby.exitLobby, {
       lobby_id: id,
     })
     await client.mutation(api.history.createHistory, {
       lobby_id: id,
-      player_id: body.player_id,
-      action_type: 'leave'
+      player_id,
+      action_type: 'leave',
     })
     return json({ success: true })
   },
 })
-
--- app/routes/api/lobbies/unregister.$id.$player_id.ts
+++ app/routes/api/lobbies/unregister.$id.$player_id.ts
@@ -3,18 +3,18 @@ import { createAPIFileRoute } from '@tanstack/start/api'
 import { api } from '~/../convex/_generated/api'
 import { client } from '~/sharedConvex'
 
-export const APIRoute = createAPIFileRoute('/api/lobbies/unregister/$id')({
-  DELETE: async ({ params, request }) => {
-    const { id } = params
-    const body = await request.json()
+export const APIRoute = createAPIFileRoute(
+  '/api/lobbies/unregister/$id/$player_id',
+)({
+  DELETE: async ({ params }) => {
+    const { id, player_id } = params
     // quit should be seperated with lobby unregister
     // check whether the user is the creator of the lobby
     const lobby = await client.query(api.lobby.getLobby, { id })
-    if (lobby && lobby.creator_id !== body.player_id) {
+    if (lobby && lobby.creator_id !== player_id) {
       return new Response("Not lobby's creator, cannot delete", { status: 400 })
     }
     await client.mutation(api.lobby.deleteLobby, { id })
     return json({ success: true })
   },
 })
-
