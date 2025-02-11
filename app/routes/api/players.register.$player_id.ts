06ae57effd2cb628ab6f6d1415b38b318e82ac95
neilbb <bn0010100@gmail.com>
2025-02-11 14:49:56 -0600

    directly return the player

--- app/routes/api/players.register.$player_id.ts
+++ app/routes/api/players.register.$player_id.ts
@@ -3,12 +3,12 @@ import { createAPIFileRoute } from '@tanstack/start/api'
 import { api } from '~/../convex/_generated/api'
 import { client } from '~/sharedConvex'
 
-export const APIRoute = createAPIFileRoute('/api/player/register/$player_id')({
+export const APIRoute = createAPIFileRoute('/api/players/register/$player_id')({
   POST: async ({ params }) => {
     const { player_id } = params
     const player = await client.mutation(api.players.createPlayer, {
       player_id,
     })
-    return json({ player })
+    return json(player)
   },
 })
