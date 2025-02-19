315369913f744cd421db112a16bbe5472b9e70fd
neilbb <bn0010100@gmail.com>
2025-02-19 16:39:25 -0600

    allow client to set player_name

--- app/routes/api/players.register.$player_id.ts
+++ app/routes/api/players.register.$player_id.ts
@@ -4,10 +4,12 @@ import { api } from '~/../convex/_generated/api'
 import { client } from '~/sharedConvex'
 
 export const APIRoute = createAPIFileRoute('/api/players/register/$player_id')({
-  POST: async ({ params }) => {
+  POST: async ({ params, request }) => {
     const { player_id } = params
+    const body = await request.json()
     const player = await client.mutation(api.players.createPlayer, {
       player_id,
+      player_name: body.player_name
     })
     return json(player)
   },
--- convex/players.ts
+++ convex/players.ts
@@ -1,10 +1,10 @@
 import { mutation } from './_generated/server'
 import { v } from 'convex/values'
-import { faker } from '@faker-js/faker'
 
 export const createPlayer = mutation({
   args: {
     player_id: v.string(),
+    player_name: v.string()
   },
   async handler(ctx, args) {
     // Check if player already exists
@@ -14,16 +14,16 @@ export const createPlayer = mutation({
       .unique()
 
     if (existingPlayer) {
+      await ctx.db.patch(existingPlayer._id, {
+        player_name: args.player_name,
+      })
       return existingPlayer
     }
-
-    // Generate a random player name using Faker
-    const player_name = faker.person.firstName()
 
     // Create new player
     const player = await ctx.db.insert('players', {
       player_id: args.player_id,
-      player_name,
+      player_name: args.player_name,
     })
 
     return player
